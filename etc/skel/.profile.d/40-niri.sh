98221cfb7b733d7152b6a175ee3383a0c51be3a6
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2026-04-14 15:43:07 -0400

    40-niri.sh: redirect to file

--- etc/skel/.profile.d/40-niri.sh
+++ etc/skel/.profile.d/40-niri.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 if [ -z "$DISPLAY" -a -z "$WAYLAND_DISPLAY" ]; then
-    case $(tty) in /dev/tty[1-3]) exec niri --session;; esac
+    case $(tty) in /dev/tty[1-3]) exec niri --session >~/.local/state/niri.log 2>&1;; esac
 fi
